3ae0052c5141ff69b7ab66a6ef279544bc0397e8
Spencer Kasper <spencer.kasper@gmail.com>
2019-10-05 17:21:42 -0500

    Basic basic logging

--- bot.js
+++ bot.js
@@ -21,145 +21,149 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    const username = DiscordHelper.getUsername(msg, client);
+    try {
+        const username = DiscordHelper.getUsername(msg, client);
 
-    if (username !== 'gamble-bot' && msg.content.startsWith("!")) {
-        let userPointsList = FileHelper.readFile(userPointsFilePath);
-        let userPoints = PointsHelper.findUserInUserPointsList(userPointsList, username);
-        const isSpencer = username === 'PoshPrincess7' ? true : false;
-        const isWilly = username === 'Gaytor' ? true : false;
-        let security = new Security(userPoints);
+        if (username !== 'gamble-bot' && msg.content.startsWith("!")) {
+            let userPointsList = FileHelper.readFile(userPointsFilePath);
+            let userPoints = PointsHelper.findUserInUserPointsList(userPointsList, username);
+            const isSpencer = username === 'PoshPrincess7' ? true : false;
+            const isWilly = username === 'Gaytor' ? true : false;
+            let security = new Security(userPoints);
 
-        if (userPoints === null || userPoints === undefined) {
-            userPoints = DiscordHelper.createNewUser(msg, username, userPointsFilePath, userPointsList);
-        }
+            if (userPoints === null || userPoints === undefined) {
+                userPoints = DiscordHelper.createNewUser(msg, username, userPointsFilePath, userPointsList);
+            }
 
-        if (msg.content === '!avoid-being-cancelled' && security.isUserAuthorized('!avoid-being-cancelled')) {
-            this.avoidCancellation = true;
-            msg.channel.send('I love gay people');
-        }
+            if (msg.content === '!avoid-being-cancelled' && security.isUserAuthorized('!avoid-being-cancelled')) {
+                this.avoidCancellation = true;
+                msg.channel.send('I love gay people');
+            }
 
-        if (msg.content === '!safe-from-being-cancelled' && security.isUserAuthorized('!safe-from-being-cancelled')) {
-            this.avoidCancellation = false;
-            msg.channel.send('Successfully avoided being cancelled');
-        }
+            if (msg.content === '!safe-from-being-cancelled' && security.isUserAuthorized('!safe-from-being-cancelled')) {
+                this.avoidCancellation = false;
+                msg.channel.send('Successfully avoided being cancelled');
+            }
 
-        if (msg.content.startsWith('!slots')) {
-            const selectedEmojiMapPath = this.avoidCancellation && (isWilly) ? './support-gays.json' : './emoji-map.json';
-            userPoints = Slots.startSlots(msg, userPoints, selectedEmojiMapPath);
-            userPointsList = PointsHelper.updateUserPointsInUserPointsList(userPointsList, userPoints);
-            FileHelper.writeFile(userPointsList, userPointsFilePath);
-        }
-
-        if (msg.content === '!points') {
-            msg.channel.send(`${username} has ${userPoints.points} total`);
-        }
-
-        if (msg.content === '!daily-points') {
-            let currentDate = new Date().setHours(0, 0, 0, 0);
-            if (userPoints.lastDailyPoints === undefined || userPoints.lastDailyPoints !== currentDate) {
-                userPoints = PointsHelper.addDailyPoints(userPoints, msg);
+            if (msg.content.startsWith('!slots')) {
+                const selectedEmojiMapPath = this.avoidCancellation && (isWilly) ? './support-gays.json' : './emoji-map.json';
+                userPoints = Slots.startSlots(msg, userPoints, selectedEmojiMapPath);
                 userPointsList = PointsHelper.updateUserPointsInUserPointsList(userPointsList, userPoints);
                 FileHelper.writeFile(userPointsList, userPointsFilePath);
-            } else {
-                msg.channel.send(`${username}, you have already claimed your daily points.  Try again tomorrow.`);
-            }
-        }
-
-        if (msg.content.split(" ")[0] === '!add-points' && security.isUserAuthorized('!add-points')) {
-            const userToGivePointsTo = msg.content.split(" ")[1];
-            const pointsToGive = Number(msg.content.split(" ")[2]);
-            const userPointsToGiveTo = PointsHelper.findUserInUserPointsList(userPointsList, userToGivePointsTo);
-            PointsHelper.addPoints(msg, pointsToGive, userPointsToGiveTo, userPointsList);
-        }
-
-        if (msg.content === "!rank") {
-            const message = RankHelper.rank(userPointsList, userPoints);
-            msg.channel.send(message);
-        }
-
-        if (msg.content === "!rank-all") {
-            const sortedUserPointsList = PointsHelper.sortUserPointsListByPoints(userPointsList);
-            const message = RankHelper.createUserPointsRankingMessage(sortedUserPointsList);
-            msg.channel.send(message);
-        }
-
-        if (msg.content.startsWith("!gift")) {
-            const usernameToGivePointsTo = msg.content.split(" ")[1];
-            const pointsToGive = Number(msg.content.split(" ")[2]);
-            let userPointsToGiveTo = PointsHelper.findUserInUserPointsList(userPointsList, usernameToGivePointsTo);
-
-            if (userPointsToGiveTo === undefined) {
-                msg.channel.send('Not a valid username.');
-            } else {
-                PointsHelper.giftPoints(msg, pointsToGive, userPointsToGiveTo, username, userPointsList);
             }
 
-        }
+            if (msg.content === '!points') {
+                msg.channel.send(`${username} has ${userPoints.points} total`);
+            }
 
-        if (msg.content === "!quit" && security.isUserAuthorized('!quit')) {
-            msg.channel.send("Shutting Down.").then(() => {
-                process.exit();
-            });
-        }
-
-        if (msg.content.startsWith("!help")) {
-            let commands = FileHelper.readFile('/../help.json').commands;
-            let message = '';
-            const pageNumber = msg.content.split(" ")[1];
-            const totalPossiblePages = parseInt(commands.length / 4);
-            if (pageNumber !== undefined) {
-                if (Number(pageNumber) !== NaN && pageNumber <= totalPossiblePages) {
-                    message = `Page Number: ${pageNumber}\n`;
-
-                    for (let i = 0; i < 4; i++) {
-                        const commandIndex = i + ((Number(pageNumber) - 1) * 4);
-                        const { name, description, usage, example, requireSpecialPermissions } = commands[commandIndex];
-                        message = message + `Command: ${name}\nDescription: ${description}\nUsage: ${usage}\nExample: ${example}\n`;
-                        message = requireSpecialPermissions ? message + 'This command requires special permissions\n\n' : message + '\n';
-                    }
-
-                    msg.channel.send(message);
-                } else if (Number(pageNumber) === totalPossiblePages + 1) {
-                    for (let i = 0; i < parseInt(commands.length % 4); i++) {
-                        const commandIndex = i + ((Number(pageNumber) - 1) * 4);
-                        const { name, description, usage, example, requireSpecialPermissions } = commands[commandIndex];
-                        message = message + `Command: ${name}\nDescription: ${description}\nUsage: ${usage}\nExample: ${example}\n`;
-                        message = requireSpecialPermissions ? message + 'This command requires special permissions\n\n' : message + '\n';
-                    }
-
-                    msg.channel.send(message);
+            if (msg.content === '!daily-points') {
+                let currentDate = new Date().setHours(0, 0, 0, 0);
+                if (userPoints.lastDailyPoints === undefined || userPoints.lastDailyPoints !== currentDate) {
+                    userPoints = PointsHelper.addDailyPoints(userPoints, msg);
+                    userPointsList = PointsHelper.updateUserPointsInUserPointsList(userPointsList, userPoints);
+                    FileHelper.writeFile(userPointsList, userPointsFilePath);
                 } else {
-                    msg.channel.send(`Invalid page number.  Total possible pages: ${totalPossiblePages + 1}`);
+                    msg.channel.send(`${username}, you have already claimed your daily points.  Try again tomorrow.`);
                 }
-            } else {
-                for (let command of commands) {
-                    const { name, description, usage, example, requireSpecialPermissions } = command;
-                    message = message + `Command: ${name}\nDescription: ${description}\nUsage: ${usage}\nExample: ${example}\n`;
-                    message = requireSpecialPermissions ? message + 'This command requires special permissions\n\n' : message + '\n';
+            }
+
+            if (msg.content.split(" ")[0] === '!add-points' && security.isUserAuthorized('!add-points')) {
+                const userToGivePointsTo = msg.content.split(" ")[1];
+                const pointsToGive = Number(msg.content.split(" ")[2]);
+                const userPointsToGiveTo = PointsHelper.findUserInUserPointsList(userPointsList, userToGivePointsTo);
+                PointsHelper.addPoints(msg, pointsToGive, userPointsToGiveTo, userPointsList);
+            }
+
+            if (msg.content === "!rank") {
+                const message = RankHelper.rank(userPointsList, userPoints);
+                msg.channel.send(message);
+            }
+
+            if (msg.content === "!rank-all") {
+                const sortedUserPointsList = PointsHelper.sortUserPointsListByPoints(userPointsList);
+                const message = RankHelper.createUserPointsRankingMessage(sortedUserPointsList);
+                msg.channel.send(message);
+            }
+
+            if (msg.content.startsWith("!gift")) {
+                const usernameToGivePointsTo = msg.content.split(" ")[1];
+                const pointsToGive = Number(msg.content.split(" ")[2]);
+                let userPointsToGiveTo = PointsHelper.findUserInUserPointsList(userPointsList, usernameToGivePointsTo);
+
+                if (userPointsToGiveTo === undefined) {
+                    msg.channel.send('Not a valid username.');
+                } else {
+                    PointsHelper.giftPoints(msg, pointsToGive, userPointsToGiveTo, username, userPointsList);
                 }
 
+            }
+
+            if (msg.content === "!quit" && security.isUserAuthorized('!quit')) {
+                msg.channel.send("Shutting Down.").then(() => {
+                    process.exit();
+                });
+            }
+
+            if (msg.content.startsWith("!help")) {
+                let commands = FileHelper.readFile('/../help.json').commands;
+                let message = '';
+                const pageNumber = msg.content.split(" ")[1];
+                const totalPossiblePages = parseInt(commands.length / 4);
+                if (pageNumber !== undefined) {
+                    if (Number(pageNumber) !== NaN && pageNumber <= totalPossiblePages) {
+                        message = `Page Number: ${pageNumber}\n`;
+
+                        for (let i = 0; i < 4; i++) {
+                            const commandIndex = i + ((Number(pageNumber) - 1) * 4);
+                            const { name, description, usage, example, requireSpecialPermissions } = commands[commandIndex];
+                            message = message + `Command: ${name}\nDescription: ${description}\nUsage: ${usage}\nExample: ${example}\n`;
+                            message = requireSpecialPermissions ? message + 'This command requires special permissions\n\n' : message + '\n';
+                        }
+
+                        msg.channel.send(message);
+                    } else if (Number(pageNumber) === totalPossiblePages + 1) {
+                        for (let i = 0; i < parseInt(commands.length % 4); i++) {
+                            const commandIndex = i + ((Number(pageNumber) - 1) * 4);
+                            const { name, description, usage, example, requireSpecialPermissions } = commands[commandIndex];
+                            message = message + `Command: ${name}\nDescription: ${description}\nUsage: ${usage}\nExample: ${example}\n`;
+                            message = requireSpecialPermissions ? message + 'This command requires special permissions\n\n' : message + '\n';
+                        }
+
+                        msg.channel.send(message);
+                    } else {
+                        msg.channel.send(`Invalid page number.  Total possible pages: ${totalPossiblePages + 1}`);
+                    }
+                } else {
+                    for (let command of commands) {
+                        const { name, description, usage, example, requireSpecialPermissions } = command;
+                        message = message + `Command: ${name}\nDescription: ${description}\nUsage: ${usage}\nExample: ${example}\n`;
+                        message = requireSpecialPermissions ? message + 'This command requires special permissions\n\n' : message + '\n';
+                    }
+
+                    msg.channel.send(message);
+                }
+            }
+
+            if (msg.content.startsWith("!blackjack")) {
+                const blackjack = new Blackjack(userPoints);
+                const command = msg.content.split(" ")[1];
+                let blackjackResults = blackjack.playBlackjack(command);
+                let message = blackjackResults.message;
+                userPoints = blackjackResults.userPoints;
+
+                if (userPoints !== undefined && userPoints !== null) {
+                    userPointsList = PointsHelper.updateUserPointsInUserPointsList(userPointsList, userPoints);
+                    FileHelper.writeFile(userPointsList, userPointsFilePath);
+                }
+
+                if (message === '' || message === null || message === undefined) {
+                    message = 'something';
+                }
                 msg.channel.send(message);
             }
         }
-
-        if (msg.content.startsWith("!blackjack")) {
-            const blackjack = new Blackjack(userPoints);
-            const command = msg.content.split(" ")[1];
-            let blackjackResults = blackjack.playBlackjack(command);
-            let message = blackjackResults.message;
-            userPoints = blackjackResults.userPoints;
-
-            if (userPoints !== undefined && userPoints !== null) {
-                userPointsList = PointsHelper.updateUserPointsInUserPointsList(userPointsList, userPoints);
-                FileHelper.writeFile(userPointsList, userPointsFilePath);
-            }
-
-            if (message === '' || message === null || message === undefined) {
-                message = 'something';
-            }
-            msg.channel.send(message);
-        }
+    } catch (error) {
+        FileHelper.writeFile(error, `./logs/error-${new Date().getTime().toString()}`)
     }
 });
 
